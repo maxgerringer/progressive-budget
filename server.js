7298d704543b213768f09a246c18a94834f3cd60
Max Gerringer <max.gerringer@gmail.com>
2020-11-05 14:43:13 -0500

    initial files and server

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Database Connection
+const URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => console.log(err)
+);
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
